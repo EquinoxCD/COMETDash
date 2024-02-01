1f00be3a731efa92653cf20ab4915d9a6dda2a38
DigiBeck <152949498+DigiBeck@users.noreply.github.com>
2024-02-01 03:56:43 -0500

    Update GetStops.js

--- GetStops.js
+++ GetStops.js
@@ -4,6 +4,6 @@ function GetStops()
 
           let stopsFile = await fetch("https://comet.mapstrat.com/current/RealTime_Temp/stops.txt");
         
-          let StopsArray = response.text().split(/\r\n |\r | \n/).map(substr => substr.split(','));
+          let StopsArray = await response.text().split(/\r\n |\r | \n/).map(substr => substr.split(','));
           return(StopsArray);
     }
