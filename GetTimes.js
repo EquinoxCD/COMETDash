e5a35cc727a2c907578cd6c736d0b457c49cd0b8
DigiBeck <152949498+DigiBeck@users.noreply.github.com>
2024-02-01 04:33:41 -0500

    Update GetTimes.js

--- GetTimes.js
+++ GetTimes.js
@@ -1,8 +1,13 @@
 async function GetTimes()
 {  
 
-  let timeFile = await fetch("https://comet.mapstrat.com/current/RealTime_Temp/stop_times.txt");
-  let timesArray = await timeFile.text().split(/\r\n|\r|\n/).map(substr => substr.split(','));
-  return(timesArray);
+  let timeFile = await fetch("https://comet.mapstrat.com/current/RealTime_Temp/stop_times.txt").then( data => 
+  {
+
+     let timesArray = await data.text().split(/\r\n|\r|\n/).map(substr => substr.split(','));
+
+
+
+  })
 
 }
